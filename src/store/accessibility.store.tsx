605ed979cbde6845470a02071bafd10ae9d4ed36
Vincenzo Ursano <vincenzo.ursano@mobimentum.it>
2019-02-06 10:36:00 +0100

    Init Accessibility => Store, keyboard events

--- src/store/accessibility.store.tsx
+++ src/store/accessibility.store.tsx
@@ -1,4 +1,18 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export enum FocusElm {
+  Controller = "focus-controller",
+  Horizontal = "focus-horizontal"
+}
+
+enum KeyMapping {
+  LEFT = 37,
+  UP = 38,
+  RIGHT = 39,
+  DOWN = 40,
+  ENTER = 13
+}
 
 export interface AccessibilityInterface {
 
@@ -13,6 +27,25 @@ class AccessibilityStoreComponent extends React.Component<any, any> {
 
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.onKeyDown.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.onKeyDown.bind(this));
+  }
+
+  onKeyDown(evt: KeyboardEvent) {
+    const direction = KeyMapping[evt.keyCode];
+    if (direction) {
+      console.log(direction);
+      const root = document.getElementById("root");
+      const buttons = root.getElementsByTagName("button");
+      console.log(buttons);
+      console.log(buttons[0].getBoundingClientRect());
+    }
   }
 
   render() {
