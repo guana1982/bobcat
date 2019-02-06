741408d378575201f23a6d125cd4ee7b4f24b5c7
Vincenzo Ursano <vincenzo.ursano@mobimentum.it>
2019-02-06 23:16:32 +0100

    Position parent

--- src/store/accessibility.store.tsx
+++ src/store/accessibility.store.tsx
@@ -14,6 +14,31 @@ enum KeyMapping {
   ENTER = 13
 }
 
+class Point {
+  x: number;
+  y: number;
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  distance(point: Point) {
+    return Math.hypot(point.x - this.x, point.y - this.y);
+  }
+
+  angle(point: Point) {
+    return Math.atan2(point.y - this.y, point.x - this.x) * 180 / Math.PI;
+  }
+
+}
+
+interface Parent {
+  element: HTMLButtonElement;
+  rect: DOMRect;
+  point?: Point;
+}
+
 export interface AccessibilityInterface {
 
 }
@@ -24,6 +49,8 @@ export const AccessibilityProvider = AccessibilityContext.Provider;
 export const AccessibilityConsumer = AccessibilityContext.Consumer;
 
 class AccessibilityStoreComponent extends React.Component<any, any> {
+
+  parents: Parent[];
 
   constructor(props) {
     super(props);
@@ -43,9 +70,37 @@ class AccessibilityStoreComponent extends React.Component<any, any> {
       console.log(direction);
       const root = document.getElementById("root");
       const buttons = root.getElementsByTagName("button");
-      console.log(buttons);
-      console.log(buttons[0].getBoundingClientRect());
+      this.setParents(Array.from(buttons));
+      this.compareParents();
     }
+  }
+
+  setParents(elements: HTMLButtonElement[]) {
+    this.parents = [];
+    elements.forEach(element => {
+      const rect: any = element.getBoundingClientRect();
+      const point = new Point(rect.x, rect.y);
+      const parent: Parent = { element, rect, point };
+      this.parents.push(parent);
+    });
+    console.log(this.parents);
+  }
+
+  compareParents() {
+    let diffPoints = [];
+    const focusParent = this.parents[5];
+    const parentList = this.parents.filter(e => e !== focusParent);
+    parentList.forEach(parent => {
+      const pointFocus = focusParent.point;
+      const point = parent.point;
+
+      const angle = pointFocus.angle(point);
+      const distance = pointFocus.distance(point);
+      const { element } = parent;
+
+      diffPoints.push({angle, distance, element});
+    });
+    console.log(diffPoints);
   }
 
   render() {
