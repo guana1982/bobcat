046fe0fa5efd730411bcbc01d23a916adad0ecf2
vinurs <vin16u@gmail.com>
2019-03-20 11:35:03 +0100

    Init Nutrition

--- src/app/core/components/global/Beverage.tsx
+++ src/app/core/components/global/Beverage.tsx
@@ -140,7 +140,7 @@ export const BeverageWrap = styled.div`
     height: 100%;
     color: ${props => props.theme.slateGrey};
     text-transform: uppercase;
-    #indicator {
+    &>#indicator {
       position: absolute;
       display: flex;
       align-items: center;
@@ -157,7 +157,7 @@ export const BeverageWrap = styled.div`
         color: #fff;
       }
     }
-    #logo, #logo-sparkling {
+    &>#logo, &>#logo-sparkling {
       visibility: hidden;
       position: absolute;
       top: 0;
@@ -166,7 +166,7 @@ export const BeverageWrap = styled.div`
       height: 220px;
       object-fit: contain;
     }
-    #title {
+    &>#title {
       position: absolute;
       font-family: NeuzeitGro-Bol;
       color: ${props => props.color};
@@ -178,7 +178,7 @@ export const BeverageWrap = styled.div`
       letter-spacing: 3.2px;
       text-align: left;
     }
-    #cal {
+    &>#cal {
       position: absolute;
       left: 23px;
       bottom: 14px;
@@ -186,7 +186,7 @@ export const BeverageWrap = styled.div`
       letter-spacing: 1px;
       text-align: left;
     }
-    #price {
+    &>#price {
       position: absolute;
       right: 23px;
       bottom: 14px;
@@ -320,6 +320,7 @@ export const Beverage = forwardRef((props: BeverageProps , innerRef: any) => {
         <BeverageWrap pouring={pouring} className={classNameBeverage} color={color}>
           <button ref={buttonEl} disabled={disabledButton}>
             <div id="element">
+              {/* <Nutrition title={title} color={color} /> */}
               {(specialCard) && <div id="indicator"><span>{type}</span></div>}
               {logoId !== undefined && <img id="logo" src={logo} />}
               {logoSparkling && <img id="logo-sparkling" src={logoSparkling} />}
@@ -368,3 +369,110 @@ export const BeveragesAnimated = [
     }
   })
 ];
+
+
+/* ==== NUTRITION ==== */
+/* ======================================== */
+
+/* color: string */
+export const NutritionWrap = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: calc(100% - 30px);
+  margin: 10px 15px;
+  font-size: 10px;
+  #title {
+    color: ${props => props.color};
+    font-family: NeuzeitGro-Bol;
+    line-height: 1.57;
+    letter-spacing: 2.5px;
+    margin: 0;
+    height: 20px;
+  }
+  .row {
+    display: block;
+    padding: 5px;
+    border-bottom: solid 2.3px #dfdfdf;
+    span {
+      display: block;
+      text-align: left;
+      &:first-child {
+        font-family: NeuzeitGro-Bol;
+      }
+    }
+  }
+  #content-values {
+    .value {
+      padding: 5px;
+      border-bottom: solid 0.5px #dfdfdf;
+      &:last-child {
+        border-bottom: none;
+      }
+      span {
+        &:nth-child(1) {
+          float: left;
+        }
+        &:nth-child(2) {
+          margin-left: 1px
+        }
+        &:nth-child(3) {
+          float: right
+        }
+      }
+    }
+  }
+`;
+
+interface NutritionProps {
+  title: string;
+  color: string;
+}
+
+export const Nutrition = (props: NutritionProps) => {
+  const { title, color } = props;
+  return (
+    <NutritionWrap color={color}>
+      <h2 id="title">{__(title)}</h2>
+      <div className="row">
+        <span>Nutrition Facts</span>
+        <span>Serving Size 0.3 fl oz (9 mL)</span>
+      </div>
+      <div className="row">
+        <span>Amount Per Serving</span>
+        <span>Calories {"0"}</span>
+      </div>
+      <div id="content-values">
+        <div className="value">
+          <span></span>
+          <span></span>
+          <span>% Daily Value *</span>
+        </div>
+        <div className="value">
+          <span>Total Fat</span>
+          <span>0g</span>
+          <span>0%</span>
+        </div>
+        <div className="value">
+          <span>Sodium</span>
+          <span>0mg</span>
+          <span>0%</span>
+        </div>
+        <div className="value">
+          <span>Potassium</span>
+          <span>35mg</span>
+          <span>1%</span>
+        </div>
+        <div className="value">
+          <span>Total Carbohydrate</span>
+          <span>0g</span>
+          <span>0%</span>
+        </div>
+        <div className="value">
+          <span>Protein</span>
+          <span>0g</span>
+          <span>0g</span>
+        </div>
+      </div>
+    </NutritionWrap>
+  );
+};
