aba211349ae84a803fe68bd25a301891372d2cbe
vinurs <vin16u@gmail.com>
2019-06-19 15:44:05 +0200

    132365 wrong passowrd => auth.tsx

--- src/app/modules/service/components/auth/Auth.tsx
+++ src/app/modules/service/components/auth/Auth.tsx
@@ -1,10 +1,13 @@
 
 import * as React from "react";
-import { ConfigContext, ServiceContext, AuthLevels } from "@core/containers";
+import { ConfigContext, ServiceContext, AuthLevels, AlertContext } from "@core/containers";
 import { ModalKeyboard, ModalKeyboardTypes } from "../common/ModalKeyboard";
 import { Pages } from "@core/utils/constants";
 import { withRouter } from "react-router-dom";
 import { Observable } from "rxjs";
+import { __ } from "@core/utils/lib/i18n";
+import { finalize } from "rxjs/operators";
+import { LoaderContext } from "@core/containers/loader.container";
 
 interface AuthProps {
   history: any;
@@ -16,22 +19,38 @@ const AuthComponent = (props: AuthProps) => {
   const configConsumer = React.useContext(ConfigContext);
 
   const { authService, setAuthService } = configConsumer;
+  const alertConsumer = React.useContext(AlertContext);
+  const loaderConsumer = React.useContext(LoaderContext);
+
   const { authLogin, setAuthLevel } = props;
 
-  React.useEffect(() => { // MOCK => START SERVICE UI
-    setAuthLevel(AuthLevels.Super);
-    props.history.push(Pages.Menu);
-    return () => {};
-  }, []);
+  // React.useEffect(() => { // MOCK => START SERVICE UI
+  //   setAuthLevel(AuthLevels.Tech);
+  //   props.history.push(Pages.Menu);
+  //   return () => {};
+  // }, []);
 
   const cancel = () => {
     setAuthService(false);
   };
 
   const finish = (output: string) => {
+    loaderConsumer.show();
     authLogin(output)
+    .pipe(
+      finalize(() => loaderConsumer.hide())
+    )
     .subscribe(
-      authLevel => props.history.push(Pages.Menu)
+      authLevel => props.history.push(Pages.Menu),
+      error => {
+        alertConsumer.show({
+          title: "WARNING",
+          content: __(error),
+          onDismiss: () => {
+            setAuthService(true);
+          }
+        });
+      }
     );
   };
 
