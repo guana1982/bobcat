99b5f63dfbd0502b39edb74700893292e0cd7122
vinurs <vin16u@gmail.com>
2019-06-26 18:14:31 +0200

    Mock for test  menu

--- src/app/core/utils/APIMock.tsx
+++ src/app/core/utils/APIMock.tsx
@@ -1,316 +1,349 @@
 export const TestMenu_ = {
   "structure_": [
-    {
+      {
       "label_id": "t_display_group_label_id",
       "elements": [
-        {
+          {
+          "label_id": "t_software_update",
+          "id": "t_software_update",
+          "type": "boolean",
+          "value": false
+          },
+          {
+          "label_id": "t_display",
           "id": "t_display",
           "type": "boolean",
-          "label_id": "t_display",
           "value": false
-        },
-        {
-          "id": "t_touch",
-          "type": "boolean",
+          },
+          {
           "label_id": "t_touch",
-          "value": false
-        },
-        {
+          "id": "t_touch",
+          "value": false,
+          "type": "boolean"
+          },
+          {
+          "label_id": "t_leds_work",
           "id": "t_leds_work",
           "type": "boolean",
-          "label_id": "t_leds_work",
           "value": false
-        },
-        {
+          },
+          {
+          "label_id": "t_trafilamenti",
           "id": "t_trafilamenti",
           "type": "boolean",
-          "label_id": "t_trafilamenti",
           "value": false
-        },
-        {
+          },
+          {
+          "label_id": "t_qr",
           "id": "t_qr",
           "type": "custom_qr",
-          "label_id": "t_qr",
           "value": false
-        },
-        {
+          },
+          {
+          "label_id": "t_proximity",
           "id": "t_proximity",
           "type": "custom_proximity",
-          "label_id": "t_proximity",
           "value": false,
           "options": [
-            {
-              "value": false,
-              "distance": 5
-            },
-            {
-              "value": false,
-              "distance": 30
-            },
-            {
-              "value": false,
-              "distance": 100
-            }
+              { "distance": 5, "value": false },
+              { "distance": 30, "value": false },
+              { "distance": 100, "value": false }
           ]
-        },
-        {
+          },
+          {
+          "label_id": "t_connectivity",
           "id": "t_connectivity",
           "type": "custom_connectivity",
-          "label_id": "t_connectivity",
           "value": false
-        },
-        {
+          },
+          {
+          "label_id": "t_power_connectivity",
           "id": "t_power_connectivity",
           "type": "custom_alarm",
-          "label_id": "t_power_connectivity",
           "value": false,
-          "alarms": [
-            "powerboard"
-          ]
-        },
-        {
+          "alarms": ["powerboard"]
+          },
+          {
+          "label_id": "t_gicar_link",
           "id": "t_gicar_link",
           "type": "custom_alarm",
-          "label_id": "t_gicar_link",
           "value": false,
-          "alarms": [
-            "gicar_connection"
-          ]
-        },
-        {
+          "alarms": ["gicar_connection"]
+          },
+          {
+          "label_id": "t_ventolina",
           "id": "t_ventolina",
           "type": "boolean",
-          "label_id": "t_ventolina",
           "value": false
-        },
-        {
+          },
+          {
+          "label_id": "t_beccuccio_centrato",
+          "id": "t_beccuccio_centrato",
+          "type": "boolean",
+          "value": false
+          },
+          {
+          "label_id": "t_spazio_bottiglia",
+          "id": "t_spazio_bottiglia",
+          "type": "boolean",
+          "value": false
+          },
+          {
+          "label_id": "t_beccuccio_protezione",
+          "id": "t_beccuccio_protezione",
+          "type": "boolean",
+          "value": false
+          },
+          {
+          "label_id": "t_accoppiamento_plastiche_telaio_lamiere",
+          "id": "t_accoppiamento_plastiche_telaio_lamiere",
+          "type": "boolean",
+          "value": false
+          },
+          {
+          "label_id": "t_accoppiamento_placchetta_sistema_pagamento",
+          "id": "t_accoppiamento_placchetta_sistema_pagamento",
+          "type": "boolean",
+          "value": false
+          },
+          {
+          "label_id": "t_accoppiamento_chiusura_porta_chiave",
+          "id": "t_accoppiamento_chiusura_porta_chiave",
+          "type": "boolean",
+          "value": false
+          },
+          {
+          "label_id": "t_materiale_corredo",
+          "id": "t_materiale_corredo",
+          "type": "boolean",
+          "value": false
+          },
+          {
+          "label_id": "t_kit_monitor",
           "id": "t_kit_monitor",
           "type": "boolean",
-          "label_id": "t_kit_monitor",
           "value": false
-        }
+          }
       ]
-    },
-    {
-      "label_id": "ada_group_label_id",
+      },
+      {
+      "label_id": "t_ada_group_label_id",
       "elements": [
-        {
+          {
+          "label_id": "t_ada_panel",
           "id": "t_ada_panel",
           "type": "custom_ada",
-          "label_id": "t_ada_panel",
           "value": false
-        }
+          }
       ]
-    },
-    {
+      },
+      {
       "label_id": "t_frigorifero_group_label_id",
       "elements": [
-        {
+          {
+          "label_id": "t_compressor",
           "id": "t_compressor",
           "type": "boolean",
-          "label_id": "t_compressor",
           "value": false
-        },
-        {
+          },
+          {
+          "label_id": "t_motoventilator",
           "id": "t_motoventilator",
           "type": "boolean",
-          "label_id": "t_motoventilator",
           "value": false
-        },
-        {
+          },
+          {
+          "label_id": "t_ntc_wokrs",
           "id": "t_ntc_wokrs",
           "type": "custom_temperature",
-          "label_id": "t_ntc_wokrs",
           "value": false
-        },
-        {
+          },
+          {
+          "label_id": "t_coldness",
           "id": "t_coldness",
           "type": "boolean",
-          "label_id": "t_coldness",
           "value": false
-        }
+          }
       ]
-    },
-    {
-      "label_id": "t_monoagitator_group_label_id",
+      },
+      {
+      "label_id": "t_motoagitator_group_label_id",
       "elements": [
-        {
+          {
+          "label_id": "t_agitator",
           "id": "t_agitator",
           "type": "boolean",
-          "label_id": "t_agitator",
           "value": false
-        }
+          }
       ]
-    },
-    {
+      },
+      {
       "label_id": "t_riempimento_vasca_group_label_id",
       "elements": [
-        {
+          {
+          "label_id": "t_statelevel_low",
           "id": "t_statelevel_low",
           "type": "boolean",
-          "label_id": "t_statelevel_low",
           "value": false
-        },
-        {
+          },
+          {
+          "label_id": "t_statelevel_high",
           "id": "t_statelevel_high",
           "type": "boolean",
-          "label_id": "t_statelevel_high",
           "value": false
-        },
-        {
+          },
+          {
+          "label_id": "t_sensor_state",
           "id": "t_sensor_state",
           "type": "custom_alarm",
-          "label_id": "t_sensor_state",
           "value": false,
-          "alarms": [
-            "level_switch"
-          ]
-        }
+          "alarms": ["level_switch"]
+          }
       ]
-    },
-    {
+      },
+      {
       "label_id": "t_test_acqua_group_label_id",
       "elements": [
-        {
+          {
+          "label_id": "t_acqua_test",
           "id": "t_acqua_test",
           "type": "custom_waters",
-          "label_id": "t_acqua_test",
           "value": false
-        }
+          }
       ]
-    },
-    {
+      },
+      {
       "label_id": "t_calibrazione_acqua_group_label_id",
       "elements": [
-        {
+          {
           "label_id": "t_calibrazione_acqua_test",
           "id": "t_calibrazione_acqua_test",
           "type": "calibration_waters",
           "value": false
-        }
+          }
       ]
-    },
-    {
+      },
+      {
       "label_id": "t_test_pompe_group_label_id",
       "elements": [
-        {
+          {
+          "label_id": "t_pompe_test",
           "id": "t_pompe_test",
           "type": "custom_bibs",
-          "label_id": "t_pompe_test",
           "value": false
-        }
+          }
       ]
-    },
-    {
+      },
+      {
       "label_id": "t_calibrazione_pompe_group_label_id",
       "elements": [
-        {
+          {
           "label_id": "t_calibrazione_pompe_test",
           "id": "t_calibrazione_pompe_test",
           "type": "calibration_pumps",
           "value": false
-        }
+          }
       ]
-    },
-    {
+      },
+      {
       "label_id": "t_pressostati_group_label_id",
       "elements": [
-        {
+          {
           "id": "t_press_co2",
           "type": "custom_press",
           "label_id": "t_press_co2",
           "value": false,
           "alarms": [
-            "press_co2"
+              "press_co2"
           ]
-        },
-        {
+          },
+          {
           "id": "t_press_h2o",
           "type": "custom_press",
           "label_id": "t_press_h2o",
           "value": false,
           "alarms": [
-            "press_h2o"
+              "press_h2o"
           ]
-        }
-      ]
+          }
+    ]
     },
-    {
+      {
       "label_id": "t_circuito_idraulico_group_label",
       "elements": [
-        {
+          {
+          "label_id": "t_elettrovalvole_erogazione",
           "id": "t_elettrovalvole_erogazione",
           "type": "boolean",
-          "label_id": "t_elettrovalvole_erogazione",
           "value": false
-        },
-        {
+          },
+          {
+          "label_id": "t_giunzioni_serpentine",
           "id": "t_giunzioni_serpentine",
           "type": "boolean",
-          "label_id": "t_giunzioni_serpentine",
           "value": false
-        },
-        {
+          },
+          {
+          "label_id": "t_giunzioni_saturatore",
           "id": "t_giunzioni_saturatore",
           "type": "boolean",
-          "label_id": "t_giunzioni_saturatore",
           "value": false
-        },
-        {
+          },
+          {
+          "label_id": "t_beccuccio_erogazione",
           "id": "t_beccuccio_erogazione",
           "type": "boolean",
-          "label_id": "t_beccuccio_erogazione",
           "value": false
-        }
+          }
       ]
-    },
-    {
+      },
+      {
       "label_id": "t_form_group_label_id",
       "elements": [
-        {
-          "type": "text",
-          "label_id": "operator_name",
+          {
+          "label_id": "t_operator_name",
           "default_value": "",
-          "permission": "write",
           "id": "operator_name",
-          "value": ""
-        },
-        {
           "type": "text",
-          "label_id": "bobcat_name",
+          "value": "",
+          "permission": "write"
+          },
+          {
+          "label_id": "t_bobcat_name",
           "default_value": "",
-          "permission": "write",
           "id": "bobcat_name",
-          "value": ""
-        },
-        {
-          "type": "password",
-          "label_id": "bobcat_name_check",
+          "type": "text",
+          "value": "",
+          "permission": "write"
+          },
+          {
+          "label_id": "t_bobcat_name_check",
           "default_value": "",
-          "permission": "write",
           "id": "bobcat_name_check",
-          "value": ""
-        }
+          "type": "password",
+          "value": "",
+          "permission": "write"
+          }
       ]
-    },
-    {
+      },
+      {
       "label_id": "t_machine_group_label_id",
       "elements": [
-        {
-          "permission": "write",
-          "type": "select",
+          {
           "label_id": "t_production_site",
           "default_value": "",
-          "options": [
-            {
-              "label": "Celli Spa, San Giovanni in Marignano",
-              "value": "Celli Spa, San Giovanni in Marignano"
-            }
-          ],
           "id": "t_production_site",
-          "value": false
-        }
+          "type": "select",
+          "value": false,
+          "options": [
+              {
+              "label": "celli_spa",
+              "value": "celli_spa"
+              }
+          ],
+          "permission": "write"
+          }
       ]
-    }
+      }
   ]
-};
+  }
