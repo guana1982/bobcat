1e38a94dad0a765bd747975fd33cc5a4817fbc13
Eric Giuliani <eric.giuliani@mobimentum.it>
2019-08-27 14:44:37 +0200

    Created p_Loading and p_ThankYou screens

--- src/app/core/containers/config.container.tsx
+++ src/app/core/containers/config.container.tsx
@@ -70,7 +70,9 @@ class ConfigStoreComponent extends React.Component<any, any> {
     /* ======================================== */
 
     const ws = webSocket({
-      url: process.env.NODE_ENV === "production" ? "ws://0.0.0.0:5901" : "ws://172.20.10.3:5901", // "ws://93.55.118.44:5901",
+      url: process.env.NODE_ENV === "production" ? "ws://0.0.0.0:5901" :
+      // "ws://172.20.10.3:5901",
+      "ws://93.55.118.36:5204",
       deserializer: data => {
         try {
           return JSON.parse(data.data);
